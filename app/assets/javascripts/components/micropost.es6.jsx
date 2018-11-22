e357a650f236acca6a2aaef327473793594e52e3
JohnnySpomer <jonispom@gmail.com>
2018-11-22 09:44:17 -0600

    added react to the application and starting creating a component

---
+++ app/assets/javascripts/components/micropost.es6.jsx
@@ -0,0 +1,9 @@
+class Micropost extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>Hello</h2>
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/server_rendering.js
@@ -0,0 +1,6 @@
+// changed this= require react-server
+// changed this= require react_ujs
+//= require ./components
+//
+// By default, this file is loaded for server-side rendering.
+// It should require your components and any dependencies.
